6f46e3dbf08c561c7310529023013df905fd8707
sergiosouzalima <sergiosouzalima@gmail.com>
2010-12-15 07:57:13 -0200

    autolink added

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -9,20 +9,25 @@ class Micropost < ActiveRecord::Base
   default_scope :order => 'microposts.created_at DESC'
   
   # Return microposts from the users being followed by the given user.
-  scope :from_users_followed_by, lambda { |user| followed_by(user) }  
+  #scope :from_users_followed_by, lambda { |user| followed_by(user) }  
+  scope :from_users_followed_by
   
   def self.from_users_followed_by(user)
     followed_ids = user.following.map(&:id).join(", ")
-    where("user_id IN (#{followed_ids}) OR user_id = ?", user)
+    if followed_ids.empty?
+      where("user_id = ?", user)
+    else
+      where("user_id IN (#{followed_ids}) OR user_id = ?", user)
+    end
   end
   
   private
   
     # Return an SQL condition for users followed by the given user.
     # We include the user's own id as well.
-    def self.followed_by(user)
-      followed_ids = %(SELECT followed_id FROM relationships
-                       WHERE follower_id = :user_id)
-      where("user_id IN (#{followed_ids}) OR user_id = :user_id",{ :user_id => user })
-    end  
+    #def self.followed_by(user)
+    #  followed_ids = %(SELECT followed_id FROM relationships
+    #                   WHERE follower_id = :user_id)                
+    #  where("user_id IN (#{followed_ids}) OR user_id = :user_id",{ :user_id => user })
+    #end  
 end
